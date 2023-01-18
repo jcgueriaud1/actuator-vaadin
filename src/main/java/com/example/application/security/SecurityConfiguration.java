e455c7c52d566dd00d052b71a4738fe2b97a29ba
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2023-01-18 22:00:20 +0200

    Add actuator and try to secure the actuator with basic auth

--- src/main/java/com/example/application/security/SecurityConfiguration.java
+++ src/main/java/com/example/application/security/SecurityConfiguration.java
@@ -4,27 +4,53 @@ import com.example.application.views.login.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
-@Configuration
-public class SecurityConfiguration extends VaadinWebSecurity {
+public class SecurityConfiguration {
+ //https://www.baeldung.com/spring-security-multiple-entry-points
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Configuration
+    @Order(2)
+    public static class App1ConfigurationAdapter {
+
+        @Bean
+        public SecurityFilterChain filterChainApp1(HttpSecurity http) throws Exception {
+            http.csrf().disable().authorizeHttpRequests()
+                    .requestMatchers(new AntPathRequestMatcher("/actuator/**")).hasRole("ADMIN")
+                    .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                    .and().httpBasic();
+            return http.build();
+        }
+
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/images/*.png")).permitAll();
-        super.configure(http);
-        setLoginView(http, LoginView.class);
+    @Configuration
+    @Order(1)
+    public static class App2ConfigurationAdapter extends VaadinWebSecurity {
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll();
+            super.configure(http);
+            setLoginView(http, LoginView.class);
+            http.logout()
+                    .invalidateHttpSession(true);
+        }
     }
 
 }
